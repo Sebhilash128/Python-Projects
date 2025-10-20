2c8a0b9aa672f0f271f2b7433ce1c07958ec4bc1
Sebhilash128 <Sebhilash128@gmail.com>
2025-10-20 14:07:26 +0530

    Add files via upload

---
+++ 99_bottles.py
@@ -0,0 +1,6 @@
+for i in range (99,0,-1):
+    print(f"{i} bottles of beer on the wall, {i} bottles of beer.")
+    print(f"Take one down and pass it around, {i-1} bottles of beer on the wall.\n")
+
+print("No more bottles of beer on the wall, no more bottles of beer.")
+print("Go to the store and buy some more, 99 bottles of beer on the wall.")
